e338469577975fbdd8e566a74052e4719b09d6b2
Thea Olivia <theaolivia.mail@gmail.com>
2016-04-07 11:38:51 +0700

    added 'milestone' attribute to Progress_Tracker.sql

---
+++ assets/mysql/Progress_Tracker.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 07, 2016 at 04:57 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `Progress_Tracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE IF NOT EXISTS `channel` (
+`id` int(11) NOT NULL,
+  `nama_channel` varchar(128) NOT NULL,
+  `id_proyek` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+CREATE TABLE IF NOT EXISTS `file` (
+`id` int(11) NOT NULL,
+  `nama_file` varchar(255) NOT NULL,
+  `waktu_upload` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_kegiatan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE IF NOT EXISTS `kegiatan` (
+`id` int(11) NOT NULL,
+  `nama_kegiatan` varchar(128) NOT NULL,
+  `deskripsi` text,
+  `waktu_mulai` date DEFAULT NULL,
+  `waktu_selesai` date DEFAULT NULL,
+  `id_proyek` int(11) NOT NULL,
+  `milestone` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE IF NOT EXISTS `pengguna` (
+  `username` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `tipe` tinyint(1) NOT NULL,
+  `jabatan` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pertemuan`
+--
+
+CREATE TABLE IF NOT EXISTS `pertemuan` (
+  `id_proyek` int(11) NOT NULL,
+  `waktu` datetime NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE IF NOT EXISTS `pesan` (
+`id` int(11) NOT NULL,
+  `konten` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_channel` int(11) NOT NULL,
+  `username_pengguna` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `progress`
+--
+
+CREATE TABLE IF NOT EXISTS `progress` (
+  `id_kegiatan` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `persentase` int(3) NOT NULL,
+  `username_pengguna` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proyek`
+--
+
+CREATE TABLE IF NOT EXISTS `proyek` (
+`id` int(11) NOT NULL,
+  `nama_proyek` varchar(128) NOT NULL,
+  `deskripsi` text,
+  `username_klien` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `channel`
+--
+ALTER TABLE `channel`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_proyek` (`id_proyek`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_kegiatan` (`id_kegiatan`);
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_proyek` (`id_proyek`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `pertemuan`
+--
+ALTER TABLE `pertemuan`
+ ADD KEY `id_proyek` (`id_proyek`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+ ADD PRIMARY KEY (`id`), ADD KEY `username_pengguna` (`username_pengguna`);
+
+--
+-- Indexes for table `progress`
+--
+ALTER TABLE `progress`
+ ADD KEY `username_pengguna` (`username_pengguna`), ADD KEY `id_kegiatan` (`id_kegiatan`);
+
+--
+-- Indexes for table `proyek`
+--
+ALTER TABLE `proyek`
+ ADD PRIMARY KEY (`id`), ADD KEY `username_klien` (`username_klien`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `channel`
+--
+ALTER TABLE `channel`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `proyek`
+--
+ALTER TABLE `proyek`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `channel`
+--
+ALTER TABLE `channel`
+ADD CONSTRAINT `channel_ibfk_1` FOREIGN KEY (`id_proyek`) REFERENCES `proyek` (`id`);
+
+--
+-- Constraints for table `file`
+--
+ALTER TABLE `file`
+ADD CONSTRAINT `file_ibfk_1` FOREIGN KEY (`id_kegiatan`) REFERENCES `kegiatan` (`id`);
+
+--
+-- Constraints for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+ADD CONSTRAINT `kegiatan_ibfk_1` FOREIGN KEY (`id_proyek`) REFERENCES `proyek` (`id`);
+
+--
+-- Constraints for table `pertemuan`
+--
+ALTER TABLE `pertemuan`
+ADD CONSTRAINT `pertemuan_ibfk_1` FOREIGN KEY (`id_proyek`) REFERENCES `proyek` (`id`);
+
+--
+-- Constraints for table `pesan`
+--
+ALTER TABLE `pesan`
+ADD CONSTRAINT `pesan_ibfk_1` FOREIGN KEY (`username_pengguna`) REFERENCES `pengguna` (`username`);
+
+--
+-- Constraints for table `progress`
+--
+ALTER TABLE `progress`
+ADD CONSTRAINT `progress_ibfk_1` FOREIGN KEY (`username_pengguna`) REFERENCES `pengguna` (`username`),
+ADD CONSTRAINT `progress_ibfk_2` FOREIGN KEY (`id_kegiatan`) REFERENCES `kegiatan` (`id`);
+
+--
+-- Constraints for table `proyek`
+--
+ALTER TABLE `proyek`
+ADD CONSTRAINT `proyek_ibfk_1` FOREIGN KEY (`username_klien`) REFERENCES `pengguna` (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
