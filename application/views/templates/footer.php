2ae5d6b1955ff4923e80f4d2aca5e8ae68eda3a1
Ahmad N. F <fahmadnaufal@gmail.com>
2016-04-13 21:23:58 +0700

    added working index page: from login.html

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+
+	<script src="<?= base_url('assets/js/jquery-2.1.4.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
---
+++ application/views/templates/html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Progress Tracker</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-theme.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/font.css') ?>">
+</head>
+<body>
